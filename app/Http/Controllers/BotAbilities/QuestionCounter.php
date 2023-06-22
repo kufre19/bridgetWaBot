c26d62b65e9f9fa4afe0691249ab4cd65af75b4f
Emmanuel <whitemaxwell5@gmail.com>
2023-06-22 21:29:30 +0100

    TEST form flow

--- app/Http/Controllers/BotAbilities/QuestionCounter.php
+++ app/Http/Controllers/BotAbilities/QuestionCounter.php
@@ -36,9 +36,11 @@ class QuestionCounter extends BotFunctionsGeneralFunctions implements AbilityInt
             case '0':
 
 
-                $qs = "Well done! You have improved your knowledge about diabetes or hypertension. 
+                $qs = <<<MSG
+                Well done! You have improved your knowledge about diabetes or hypertension. 
                 I appreciate your efforts. How do you want to continue your journey to beat diabetes or 
-                hypertension? Can we plan together to beat diabetes or hypertension? ";
+                hypertension? Can we plan together to beat diabetes or hypertension?
+                MSG;
                 $header = "Improved your knowledge";
                 $btn = [
                     [
@@ -64,22 +66,28 @@ class QuestionCounter extends BotFunctionsGeneralFunctions implements AbilityInt
                 // check if user responded with btn yes/no or text y/n if not any of it repeat the last one
                 $user_response = $this->button_id ?? $this->user_message_lowered;
                 if ($user_response == "yes") {
-                    $message = "Thank you for working with me to beat these risk factors? What concerns do 
-                    you have with your blood sugar or blood pressure readings? ";
+                    $message =  <<<MSG
+                    Thank you for working with me to beat these risk factors? What concerns do 
+                    you have with your blood sugar or blood pressure readings?
+                    MSG;
                     $data  = $this->make_text_message($message);
                     $this->send_post_curl($data);
                     $ask_qs = true;
                 }
 
                 if ($user_response == "no") {
-                    $message = "Thank you. I understand that you are not ready to plan yet. Will you like to keep learning? 
-                    Share your concerns with me by asking a question.";
+                    $message =  <<<MSG
+                    Thank you. I understand that you are not ready to plan yet. Will you like to keep learning? 
+                    Share your concerns with me by asking a question
+                    MSG;
                     $data  = $this->make_text_message($message);
                     $this->send_post_curl($data);
                     $this->update_session();
                     $this->ResponsedWith200();
                 }else{
-                    $message = "sorry i did not understand the response please can you repeat that again";
+                    $message =  <<<MSG
+                    sorry i did not understand the response please can you repeat that again
+                    MSG;
                     $data  = $this->make_text_message($message);
                     $this->send_post_curl($data);
                     $repeat_last_action = true;
@@ -89,24 +97,30 @@ class QuestionCounter extends BotFunctionsGeneralFunctions implements AbilityInt
                 break;
 
             case '2':
-                $message = "I appreciate your bravery to share your concerns with me today. What actions do you want to take 
-                to improve your blood sugar and/or blood pressure readings?";
+                $message =  <<<MSG
+                I appreciate your bravery to share your concerns with me today. What actions do you want to take 
+                to improve your blood sugar and/or blood pressure readings?
+                MSG;
                 $data  = $this->make_text_message($message);
                 $this->send_post_curl($data);
                 $ask_qs = true;
                 break;
 
             case '3':
-                $message = "Thank you for choosing to work with me to address your concerns about diabetes or hypertension today. I understand 
-                that you want to make changes to improve your wellbeing.  ";
+                $message = <<<MSG
+                Thank you for choosing to work with me to address your concerns about diabetes or hypertension today. I understand 
+                that you want to make changes to improve your wellbeing.
+                MSG;
                 $data  = $this->make_text_message($message);
                 $this->send_post_curl($data);
                 break;
 
             case '4':
-                $message = "Lifestyle changes that keep blood pressure or blood sugar within normal limits involve healthy diets, increased physical activity, weight management, 
+                $message = <<<MSG
+                Lifestyle changes that keep blood pressure or blood sugar within normal limits involve healthy diets, increased physical activity, weight management, 
                 stress management and use of medications. You need all of these to achieve normal readings. 
-                Can I share ideas on how you can achieve these lifestyle changes?";
+                Can I share ideas on how you can achieve these lifestyle changes?
+                MSG;
                 $data  = $this->make_text_message($message);
                 $this->send_post_curl($data);
                 $ask_qs = true;
@@ -114,9 +128,11 @@ class QuestionCounter extends BotFunctionsGeneralFunctions implements AbilityInt
         }
 
         if ($form_counter = 4) {
-            $message = "Thank you for taking this great step today. I will send you tips on how to 
+            $message = <<<MSG
+            Thank you for taking this great step today. I will send you tips on how to 
             achieve these lifestyle changes.  
-            You can keep asking more questions about these risk factors";
+            You can keep asking more questions about these risk factors
+            MSG;
             $data  = $this->make_text_message($message);
             $this->send_post_curl($data);
             $this->update_session();
