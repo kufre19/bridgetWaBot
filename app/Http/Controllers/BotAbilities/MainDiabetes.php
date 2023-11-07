a9b44b4c1f36b0747df4ee97d003a49d2a9f92da
Emmanuel kufre <whitemaxwell5@gmail.com>
2023-11-07 20:32:08 +0100

    don't repeat authorized numbers

--- app/Http/Controllers/BotAbilities/MainDiabetes.php
+++ app/Http/Controllers/BotAbilities/MainDiabetes.php
@@ -103,13 +103,17 @@ class MainDiabetes extends BotFunctionsGeneralFunctions implements AbilityInterf
     public function updateQuestionProgress($question)
     {
         $question_progress = $this->user_session_data['question_progress']['diabetes'];
-        $old_corresponding = $question->corresponding_number;
-        $new_corresponding = $old_corresponding + 1;
-        $question_progress['questions_asked'][] = $new_corresponding;
-
-        $new_session = $this->user_session_data;
-        $new_session['question_progress']['diabetes'] = $question_progress;
-        $this->update_session($new_session);
+        if (!in_array($this->user_message_original, $question_progress['questions_asked']))
+        {
+            $old_corresponding = $question->corresponding_number;
+            $new_corresponding = $old_corresponding + 1;
+            $question_progress['questions_asked'][] = $new_corresponding;
+    
+            $new_session = $this->user_session_data;
+            $new_session['question_progress']['diabetes'] = $question_progress;
+            $this->update_session($new_session);
+        }
+       
     }
 
     public function getQuestionProgress()
